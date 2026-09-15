ab64b87a29db2594b2026515bdbfc3e0984cf31e
Mike <mihalispro@gmail.com>
2026-09-16 00:06:50 +0300

    testing

--- src/main/java/com/example/springbootdemo/api/model/User.java
+++ src/main/java/com/example/springbootdemo/api/model/User.java
@@ -1,4 +1,34 @@
 package com.example.springbootdemo.api.model;
 
 public class User {
+
+    private String name;
+    private int id;
+    private int age;
+    private String email;
+
+    public User(String name, int id, int age, String email) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+
 }
